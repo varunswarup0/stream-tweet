acce9ab6b986d51fa0a480b80082d6bc2ff5d022
varunswarup0 <varunswarup0@gmail.com>
2021-07-14 04:29:10 +0530

    Updates

---
+++ precache-manifest.fb889996b824239b8d9cd71477255f5d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89a359939fcd91da942b8b0d8929bd52",
+    "url": "/stream-tweet/index.html"
+  },
+  {
+    "revision": "70ecd8373e69678e96c0",
+    "url": "/stream-tweet/static/css/main.7f1e4b90.chunk.css"
+  },
+  {
+    "revision": "0e536d462eb58cf7a904",
+    "url": "/stream-tweet/static/js/2.76c8c50e.chunk.js"
+  },
+  {
+    "revision": "70ecd8373e69678e96c0",
+    "url": "/stream-tweet/static/js/main.52eb2e8e.chunk.js"
+  },
+  {
+    "revision": "39e1bb7ad1014788df2a",
+    "url": "/stream-tweet/static/js/runtime~main.6261c9c1.js"
+  }
+]);
